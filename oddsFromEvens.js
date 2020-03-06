f5ad788bf1b4aa2d925231b235c2e252da52058b
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-05 21:31:06 -0500

    got it

--- oddsFromEvens.js
+++ oddsFromEvens.js
@@ -1,11 +1,20 @@
 //ODDS FROM EVENS
-
-const oddsFromEvens = (arr) => {
-
-}
-
-console.log(oddsFromEvens([423, 322, 918, 45, 6, 79, 55]))
-
+const oddsFromEvens = arr => {
+  let odd = [];
+  let even = [];
+  let returnArr = [];
+  let sortArr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < sortArr.length; i++)
+    if (sortArr[i] % 2 === 0) {
+      even.push(sortArr[i]);
+    } else {
+      odd.push(sortArr[i]);
+    }
+  let oddReverse = odd.reverse();
+  returnArr = even.concat(oddReverse);
+  return returnArr;
+};
+// console.log(oddsFromEvens([423, 322, 918, 45, 6, 79, 55]));
 module.exports = {
   oddsFromEvens
-}
+};
